7fd3dd0d43b7e8e3b02d0eeed169acb71c7e80f8
04M04 <micsch04@gmail.com>
2023-04-15 01:30:53 +0200

    cleanup safe

--- Device/Device.ino
+++ Device/Device.ino
@@ -12,9 +12,14 @@
 #define DISPLAYS_DOWN 3
 DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
 
-   //Timer setup
-  //create a hardware timer  of ESP32
-  hw_timer_t * timer = NULL;
+//Timer setup
+//create a hardware timer  of ESP32
+hw_timer_t * timer = NULL;
+
+int team1_points = 0;
+int team2_points = 0;
+int team1_plays = 0;
+int team2_plays = 0;
 
 /*--------------------------------------------------------------------------------------
   Interrupt handler for Timer1 (TimerOne) driven DMD refresh scanning, this gets
@@ -25,27 +30,21 @@ void IRAM_ATTR triggerScan()
   dmd.scanDisplayBySPI();
 }
 
-// void drawCount()
-// {
-//   //int to string
-//   int team1_points = 1;
-//   int team2_points = 2;
-//   int team1_plays = 3;
-//   int team2_plays = 4;
+void drawCount(int team1_points, int team2_points, int team1_plays, int team2_plays)
+{
+  //draw Count
+  dmd.selectFont(myFont_16x32);
+  dmd.drawChar( 0, 0, char(team1_points), GRAPHICS_NORMAL );
+  dmd.drawChar(16, 0, char(team2_points), GRAPHICS_NORMAL );
+  dmd.drawChar(32, 0, char(team1_plays), GRAPHICS_NORMAL );
+  dmd.drawChar(48, 0, char(team2_plays), GRAPHICS_NORMAL );
 
-//   //draw Count
-//   dmd.selectFont(myFont_16x32);
-//   dmd.drawChar( 0, 0, char(team1_points), GRAPHICS_NORMAL );
-//   dmd.drawChar(16, 0, char(team2_points), GRAPHICS_NORMAL );
-//   dmd.drawChar(32, 0, char(team1_plays), GRAPHICS_NORMAL );
-//   dmd.drawChar(48, 0, char(team2_plays), GRAPHICS_NORMAL );
-
-//   dmd.selectFont(myFont_16x16);
-//   dmd.drawChar( 0, 32, char(team1_points), GRAPHICS_NORMAL );
-//   dmd.drawChar(16, 32, char(team2_points), GRAPHICS_NORMAL );
-//   dmd.drawChar(32, 32, char(team1_plays), GRAPHICS_NORMAL );
-//   dmd.drawChar(48, 32, char(team2_plays), GRAPHICS_NORMAL );
-// }
+  dmd.selectFont(myFont_16x16);
+  dmd.drawChar( 0, 32, char(team1_points), GRAPHICS_NORMAL );
+  dmd.drawChar(16, 32, char(team2_points), GRAPHICS_NORMAL );
+  dmd.drawChar(32, 32, char(team1_plays), GRAPHICS_NORMAL );
+  dmd.drawChar(48, 32, char(team2_plays), GRAPHICS_NORMAL );
+}
 
 /*--------------------------------------------------------------------------------------
   setup
@@ -82,16 +81,31 @@ void loop(void)
 {
   //drawCount();
 
-  //int to string
-  int team1_points = 1;
-  int team2_points = 2;
-  int team1_plays = 3;
-  int team2_plays = 4;
+  // //int to string
+  // int team1_points = 1;
+  // int team2_points = 2;
+  // int team1_plays = 3;
+  // int team2_plays = 4;
 
-  //draw Count
+  // //convert int to char
+  // char team1_points_char = team1_points;
+  // char team2_points_char = team2_points;
+  // char team1_plays_char = team1_plays;
+  // char team2_plays_char = team2_plays;
+
+  int a = 17;    /*Initialize a string with value 1234*/
+
+
+
   dmd.selectFont(myFont_16x32);
-  dmd.drawChar( 0, 0, team1_points, GRAPHICS_NORMAL );
-  dmd.drawChar(16, 0, team2_points, GRAPHICS_NORMAL );
-  dmd.drawChar(32, 0, team1_plays, GRAPHICS_NORMAL );
-  dmd.drawChar(48, 0, team2_plays, GRAPHICS_NORMAL );
+  dmd.drawChar( 0, 0, String(a)[0], GRAPHICS_NORMAL );
+  dmd.drawChar(16, 0, String(a)[1], GRAPHICS_NORMAL );
+  dmd.drawChar(32, 0, '0', GRAPHICS_NORMAL );
+  dmd.drawChar(48, 0, '0', GRAPHICS_NORMAL );
+
+  dmd.selectFont(myFont_16x16);
+  dmd.drawChar( 0, 32, '0', GRAPHICS_NORMAL );
+  dmd.drawChar(16, 32, '0', GRAPHICS_NORMAL );
+  dmd.drawChar(32, 32, '0', GRAPHICS_NORMAL );
+  dmd.drawChar(48, 32, '0', GRAPHICS_NORMAL );
 }
