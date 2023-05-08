aa2d5a1b14b33535188edf2d69aa52999e756801
04M04 <micsch04@gmail.com>
2023-05-08 16:44:33 +0200

    add button functionality

--- Control/matdis_control/lib/presentation/points/widgets/custom_points_field_vertical.dart
+++ Control/matdis_control/lib/presentation/points/widgets/custom_points_field_vertical.dart
@@ -5,69 +5,84 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class CustomPointsFieldVertical extends StatelessWidget {
   final Color color;
   final int points;
+  final Function onReset;
+  final Function onAdd;
+  final Function onRemove;
   const CustomPointsFieldVertical(
-      {super.key, required this.color, required this.points});
+      {super.key,
+      required this.onReset,
+      required this.onAdd,
+      required this.onRemove,
+      required this.color,
+      required this.points});
 
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
 
-    return InkResponse(
-      // onTap: () =>
-      //     BlocProvider.of<AdvicerBloc>(context).add(AdviceRequestedEvent()),
+    return Container(
+      width: 160,
+      height: 280,
       child: Stack(
         clipBehavior: Clip.none,
-        // overflow: Overflow.visible,
         children: [
-          Card(
-            elevation: 1,
-            color: color,
-            child: Container(
-                width: 120,
-                height: 120,
-                child: Center(
-                  child: Text(
-                    points.toString(),
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.normal),
+          Center(
+            child: InkResponse(
+              onLongPress: () => onReset(),
+              child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: color,
                   ),
-                )),
-          ),
-          const Positioned(
-              top: -40,
-              left: 30,
-              child: Card(
-                elevation: 3,
-                color: Colors.white70,
-                child: SizedBox(
-                  width: 60,
-                  height: 60,
+                  width: 160,
+                  height: 160,
+                  // color: color,
                   child: Center(
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                      size: 40,
+                    child: Text(
+                      points.toString(),
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 70,
+                          fontWeight: FontWeight.normal),
                     ),
+                  )),
+            ),
+          ),
+          Positioned(
+              top: 0,
+              left: 30,
+              child: InkResponse(
+                onTap: () => onAdd(),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white60,
+                  ),
+                  width: 100,
+                  height: 100,
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 70,
                   ),
                 ),
               )),
-          const Positioned(
-            bottom: -40,
+          Positioned(
+            bottom: 0,
             left: 30,
-            child: Card(
-              elevation: 3,
-              color: Colors.white38,
-              child: SizedBox(
-                width: 60,
-                height: 60,
-                child: Center(
-                  child: Icon(
-                    Icons.remove,
-                    color: Colors.white,
-                    size: 40,
-                  ),
+            child: InkResponse(
+              onTap: () => onRemove(),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white38,
+                ),
+                width: 100,
+                height: 100,
+                child: const Icon(
+                  Icons.remove,
+                  color: Colors.white,
+                  size: 70,
                 ),
               ),
             ),
